9b19d5d62ed67643e0da62a3440facb40cd6a151
mohamednaffeti <mohamednaffati08@gmail.com>
2023-09-03 22:52:41 +0100

    conditionals

--- src/Main.java
+++ src/Main.java
@@ -35,8 +35,51 @@ public class Main {
         List<String> listName= new ArrayList<String>();
         System.out.println(listName);
 
-        // i invite you to take a look on the file in discrod ( difference between
-        // primitive and reference data types)
+        /* I invite you to take a look on the file in discrod ( difference between
+         primitive and reference data types)
+         PS: All reference types are a subclass of type java.lang.Object It contains
+         the address (or reference) of dynamically created objects
+         */
+
+        //CONDITIONALS
+        /*
+        in this state , we have 4 types of consitionals that we need to learn
+        1 : if statement
+        2 : if-else statement
+        3 : if else-if ... else ladder
+        4 : nested if statement
+        5 : using ternary operator
+        */
+
+        // 1 : if statement Example:
+        int note = 15;
+        if (note > 10)
+            System.out.println("Accepted");
+        //2 : if-else statement
+        if (note > 10){
+            System.out.println("Accepted");}
+        else {
+            System.out.println("Not Accepted");
+        }
+        //3 : if else-if ... else ladder
+        if (note > 10){
+            System.out.println("Accepted");}
+        else if (note >= 9 && note <10){
+            System.out.println("you need to pass the control session");
+        }else {
+            System.out.println("refused");
+        }
+        //4 : nested if statement
+        // Adding new test variable
+        String subject="java";
+        if (note>10){
+            if (subject.equals("java")){
+                System.out.println("You are accepted");
+            }
+        }
+        //5 : using ternary operator
+        String Result = (note>10)? "you are accepted":"you are not accepted";
+        System.out.println(Result);
 
 
 
