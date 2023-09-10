0c55a3c4760bbfc5db4d74dc5a055dda583a0117
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2023-09-10 13:02:09 +0200

    Basic Syntax , Data Types and Variables

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("hello Fathi");
-        System.out.println("test");
+
+    //Data Types
+    //Primitive data type :
+    // Bytes :  8-bit integer from -128 to 127
+     byte myByteVariable=2;
+     // short: 16-bit integer from -32,768 to 32,767
+     short myShortVariable=5;
+     // int: 32-bit integer from -2147483648 to 2147483647
+     int myIntVariable =5;
+     long myLongVariable=30;
+     // you have to use the f after Initializing the variable
+     //float  is a 32-bit floating-point data type
+     float myFloatVariable=5.7f;
+     //double is a 64-bit floating-point data type
+     double myDoubleVariable=3;
+     char myCharVariable='c';
+     //Reference Data Types:
+     // Classes
+     class  myFirstClass{}
+     // Interfaces()
+     //  interface myFirstInterface{}
+      // Enums
+      // enum myFirstEnum{
+
+     String myStringVariable="hello";
+     // arrays
+     int [] codes=new int [5];
+
+
+
+
+
+
+
+
     }
 
 
